04b9a7403ca8aabf8430e8a391948bf45581b07d
Lans12345 <olanalans12345@gmail.com>
2023-10-19 18:39:43 +0800

    added plant description

--- lib/screens/tabs/plant_tab.dart
+++ lib/screens/tabs/plant_tab.dart
@@ -17,14 +17,100 @@ class PlantTab extends StatelessWidget {
           },
           itemBuilder: (context, index) {
             return ListTile(
-              leading: Container(
-                width: 75,
-                height: 75,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                        'assets/images/${herbalPlants[index].commonName}.jpg'),
+              leading: GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return SizedBox(
+                        height: 500,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Align(
+                                  alignment: Alignment.topRight,
+                                  child: IconButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    icon: const Icon(
+                                      Icons.close,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  width: double.infinity,
+                                  height: 150,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(
+                                          'assets/images/${herbalPlants[index].commonName}.jpg'),
+                                    ),
+                                  ),
+                                ),
+                                const Divider(),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                TextWidget(
+                                  text: herbalPlants[index].commonName,
+                                  fontSize: 24,
+                                  fontFamily: 'Bold',
+                                  color: Colors.black,
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                TextWidget(
+                                  text:
+                                      'Scientific name: ${herbalPlants[index].scientificName}',
+                                  fontSize: 18,
+                                  fontFamily: 'Bold',
+                                  color: Colors.black,
+                                ),
+                                TextWidget(
+                                  text:
+                                      'Plant description: ${herbalPlants[index].description}',
+                                  fontSize: 14,
+                                  fontFamily: 'Regular',
+                                  color: Colors.grey,
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                TextWidget(
+                                  text:
+                                      'Guides:  ${herbalPlants[index].plantingGuide}',
+                                  fontSize: 14,
+                                  fontFamily: 'Regular',
+                                  color: Colors.grey,
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  width: 75,
+                  height: 75,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                          'assets/images/${herbalPlants[index].commonName}.jpg'),
+                    ),
                   ),
                 ),
               ),
