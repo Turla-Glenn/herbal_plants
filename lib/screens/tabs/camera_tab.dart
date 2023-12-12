5f7f388bf901b5a4085df1706d2c8733e9ddc41a
Lans12345 <olanalans12345@gmail.com>
2023-12-13 07:48:20 +0800

    added camera func

--- lib/screens/tabs/camera_tab.dart
+++ lib/screens/tabs/camera_tab.dart
@@ -1,10 +1,136 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class CameraTab extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:herbal_plants/widgets/text_widget.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_v2/tflite_v2.dart';
+
+class CameraTab extends StatefulWidget {
   const CameraTab({super.key});
 
   @override
+  State<CameraTab> createState() => _CameraTabState();
+}
+
+class _CameraTabState extends State<CameraTab> {
+  late String output = '';
+
+  late File pickedImage;
+
+  bool isImageLoaded = false;
+
+  late List result;
+
+  late String accuracy = '';
+
+  late String name = '';
+
+  late String numbers = '';
+
+  getImageCamera(String imgsrc) async {
+    setState(() {
+      hasLoaded = false;
+    });
+    var tempStore = await ImagePicker().pickImage(
+      source: imgsrc == 'camera' ? ImageSource.camera : ImageSource.gallery,
+    );
+
+    setState(() {
+      pickedImage = File(tempStore!.path);
+      isImageLoaded = true;
+      applyModel(File(tempStore.path));
+      hasLoaded = true;
+    });
+  }
+
+  List works = [];
+
+  loadmodel() async {
+    try {
+      await Tflite.loadModel(
+        model: "assets/model/model_unquant.tflite",
+        labels: "assets/model/labels.txt",
+      );
+    } catch (e) {
+      print('error $e');
+    }
+
+    // works = jsonDecode(await rootBundle.loadString('assets/data/main.json'));
+
+    setState(() {
+      hasLoaded = true;
+    });
+  }
+
+  String str = '';
+
+  applyModel(File file) async {
+    var res = await Tflite.runModelOnImage(
+        path: file.path, // required
+        imageMean: 0.0, // defaults to 117.0
+        imageStd: 255.0, // defaults to 1.0
+        numResults: 2, // defaults to 5
+        threshold: 0.2, // defaults to 0.1
+        asynch: true);
+    setState(() {
+      print('res $res');
+      result = res!;
+      print('result:$result');
+      str = '${result[0]['label']}';
+    });
+
+    print(str);
+  }
+
+  bool hasLoaded = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    loadmodel();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              getImageCamera('gallery');
+            },
+            child: Card(
+              child: Container(
+                height: 250,
+                width: 250,
+                decoration: const BoxDecoration(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.qr_code_scanner,
+                      color: Colors.blue,
+                      size: 75,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextWidget(
+                      text: 'Scan Plant',
+                      fontSize: 32,
+                      color: Colors.blue,
+                      fontFamily: 'Bold',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
