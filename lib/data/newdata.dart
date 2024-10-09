aa5383c3f6c3ad387793775ad51e861d1880d125
Lans12345 <olanalans12345@gmail.com>
2024-10-09 13:57:04 +0800

    added disease tab data

--- lib/data/newdata.dart
+++ lib/data/newdata.dart
@@ -37,3 +37,86 @@ List<Map<String, dynamic>> crops = [
     'harvestTime': '80 to 90 days after planting, depending on the variety.'
   }
 ];
+
+List<Map<String, dynamic>> riceDiseases = [
+  {
+    'name': 'Bacterial Leaf Blight',
+    'description':
+        'A bacterial disease caused by Xanthomonas oryzae that affects the leaves of rice, causing wilting and yellowing.',
+    'prevention':
+        'Use resistant varieties, ensure proper field drainage, and avoid excessive use of nitrogen fertilizers.'
+  },
+  {
+    'name': 'Brown Spot',
+    'description':
+        'A fungal disease caused by Bipolaris oryzae, resulting in brown lesions on leaves, leading to poor grain filling.',
+    'prevention':
+        'Apply balanced fertilizers, improve drainage, and use fungicide treatments when necessary.'
+  },
+  {
+    'name': 'Leaf Smut',
+    'description':
+        'A fungal disease caused by Entyloma oryzae, which causes black or grayish lesions on leaves, reducing photosynthesis.',
+    'prevention':
+        'Use resistant varieties, apply fungicides, and practice good field hygiene to prevent spread.'
+  }
+];
+
+List<Map<String, dynamic>> cornDiseases = [
+  {
+    'name': 'Blight',
+    'description':
+        'A fungal disease caused by Bipolaris maydis or other pathogens, leading to the development of large brown spots on the leaves and reducing crop yields.',
+    'prevention':
+        'Use resistant varieties, practice crop rotation, and avoid planting corn in infected fields for a few years.'
+  },
+  {
+    'name': 'Common Rust',
+    'description':
+        'A fungal disease caused by Puccinia sorghi, characterized by small, reddish-brown pustules on both sides of the leaves.',
+    'prevention':
+        'Plant rust-resistant corn varieties, apply fungicides, and avoid dense plantings to reduce humidity.'
+  },
+  {
+    'name': 'Gray Leaf Spot',
+    'description':
+        'A fungal disease caused by Cercospora zeae-maydis, resulting in rectangular, gray to tan lesions on leaves.',
+    'prevention':
+        'Plant resistant hybrids, practice crop rotation, and remove plant debris from fields to reduce the source of infection.'
+  },
+  {
+    'name': 'Healthy',
+    'description': 'No disease present; the crop is healthy and thriving.',
+    'prevention':
+        'Continue best practices in crop management, including proper watering, nutrient balance, and disease prevention methods.'
+  }
+];
+List<Map<String, dynamic>> watermelonDiseases = [
+  {
+    'name': 'Anthracnose',
+    'description':
+        'A fungal disease caused by Colletotrichum orbiculare, resulting in dark, sunken lesions on leaves, stems, and fruits.',
+    'prevention':
+        'Use disease-free seeds, apply fungicides, rotate crops, and avoid overhead irrigation to reduce moisture on leaves.'
+  },
+  {
+    'name': 'Downy Mildew',
+    'description':
+        'A fungal disease caused by Pseudoperonospora cubensis, causing yellow patches on leaves that turn brown and die.',
+    'prevention':
+        'Use resistant varieties, apply fungicides, and avoid planting in areas with poor air circulation or excess moisture.'
+  },
+  {
+    'name': 'Mosaic Virus',
+    'description':
+        'A viral disease that causes yellow mottling and distortion of leaves, leading to stunted growth and deformed fruits.',
+    'prevention':
+        'Use virus-free seeds, control insect vectors (such as aphids), and remove infected plants immediately.'
+  },
+  {
+    'name': 'Healthy',
+    'description': 'No disease present; the crop is healthy and growing well.',
+    'prevention':
+        'Maintain regular monitoring and use good agricultural practices to keep the crop healthy.'
+  }
+];
--- lib/screens/tabs/plant_tab.dart
+++ lib/screens/tabs/plant_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:herbal_plants/data/newdata.dart';
 import 'package:herbal_plants/data/plants.dart';
 import 'package:herbal_plants/widgets/text_widget.dart';
 
@@ -9,125 +10,127 @@ class PlantTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-      child: SizedBox(
-        child: ListView.separated(
-          itemCount: herbalPlants.length,
-          separatorBuilder: (context, index) {
-            return const Divider();
-          },
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: GestureDetector(
-                onTap: () {
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (context) {
-                      return SizedBox(
-                        height: 500,
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SingleChildScrollView(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Align(
-                                  alignment: Alignment.topRight,
-                                  child: IconButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    icon: const Icon(
-                                      Icons.close,
-                                      color: Colors.red,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  width: double.infinity,
-                                  height: 150,
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: AssetImage(
-                                          'assets/images/${herbalPlants[index].commonName}.jpg'),
-                                    ),
-                                  ),
-                                ),
-                                const Divider(),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                TextWidget(
-                                  text: herbalPlants[index].commonName,
-                                  fontSize: 24,
-                                  fontFamily: 'Bold',
-                                  color: Colors.black,
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                TextWidget(
-                                  text:
-                                      'Scientific name: ${herbalPlants[index].scientificName}',
-                                  fontSize: 18,
-                                  fontFamily: 'Bold',
-                                  color: Colors.black,
-                                ),
-                                TextWidget(
-                                  text:
-                                      'Plant description: ${herbalPlants[index].description}',
-                                  fontSize: 14,
-                                  fontFamily: 'Regular',
-                                  color: Colors.grey,
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                TextWidget(
-                                  text:
-                                      'Guides:  ${herbalPlants[index].plantingGuide}',
-                                  fontSize: 14,
-                                  fontFamily: 'Regular',
-                                  color: Colors.grey,
-                                ),
-                                const SizedBox(
-                                  height: 20,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-                child: Container(
-                  width: 75,
-                  height: 75,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                          'assets/images/${herbalPlants[index].commonName}.jpg'),
-                    ),
-                  ),
-                ),
-              ),
-              title: TextWidget(
-                text: herbalPlants[index].commonName,
-                fontSize: 18,
-                fontFamily: 'Bold',
-              ),
-              subtitle: TextWidget(
-                text: herbalPlants[index].description,
-                fontSize: 12,
-                fontFamily: 'Regular',
-              ),
-            );
-          },
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Column(
+              children: list1(context, cornDiseases, 'corn', 'Corn'),
+            ),
+            Column(
+              children: list1(context, riceDiseases, 'rice', 'Rice'),
+            ),
+            Column(
+              children: list1(
+                  context, watermelonDiseases, 'watermelon', 'Watermelon'),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  List<Widget> list1(context, List disease, String img, String type) {
+    return [
+      for (int i = 0; i < disease.length; i++)
+        ListTile(
+          leading: GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return SizedBox(
+                    height: 500,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: IconButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                icon: const Icon(
+                                  Icons.close,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: double.infinity,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage('assets/images/$img.PNG'),
+                                ),
+                              ),
+                            ),
+                            const Divider(),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextWidget(
+                              text: disease[i]['name'],
+                              fontSize: 24,
+                              fontFamily: 'Bold',
+                              color: Colors.black,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextWidget(
+                              text:
+                                  'Disease description: ${disease[i]['description']}',
+                              fontSize: 14,
+                              fontFamily: 'Regular',
+                              color: Colors.grey,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextWidget(
+                              text: 'Prevention:  ${disease[i]['prevention']}',
+                              fontSize: 14,
+                              fontFamily: 'Regular',
+                              color: Colors.grey,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            child: Container(
+              width: 75,
+              height: 75,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/$img.PNG'),
+                ),
+              ),
+            ),
+          ),
+          title: TextWidget(
+            text: disease[i]['name'] + ' ($type)',
+            fontSize: 18,
+            fontFamily: 'Bold',
+          ),
+          subtitle: TextWidget(
+            text: disease[i]['description'],
+            fontSize: 12,
+            fontFamily: 'Regular',
+          ),
+        )
+    ];
   }
 }
